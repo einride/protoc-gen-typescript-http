bc35fae77cda05a5f309aa1fa2cd1b4b9ce482af
ericwenn <ericwenn@users.noreply.github.com>
2021-03-06 14:46:59 +0100

    feat: support map fields

---
+++ internal/plugin/type.go
@@ -0,0 +1,65 @@
+package plugin
+
+import "google.golang.org/protobuf/reflect/protoreflect"
+
+type Type struct {
+	IsNamed bool
+	Name    string
+
+	IsList     bool
+	IsMap      bool
+	Underlying *Type
+}
+
+func (t Type) Reference() string {
+	switch {
+	case t.IsMap:
+		return "{ [key: string]: " + t.Underlying.Reference() + "}"
+	case t.IsList:
+		return t.Underlying.Reference() + "[]"
+	default:
+		return t.Name
+	}
+}
+
+func typeFromField(field protoreflect.FieldDescriptor) Type {
+	switch {
+	case field.IsMap():
+		underlying := namedTypeFromField(field.MapValue())
+		return Type{
+			IsMap:      true,
+			Underlying: &underlying,
+		}
+	case field.IsList():
+		underlying := namedTypeFromField(field)
+		return Type{
+			IsList:     true,
+			Underlying: &underlying,
+		}
+	default:
+		return namedTypeFromField(field)
+	}
+}
+
+func namedTypeFromField(field protoreflect.FieldDescriptor) Type {
+	switch field.Kind() {
+	case protoreflect.StringKind, protoreflect.BytesKind:
+		return Type{IsNamed: true, Name: "string"}
+	case protoreflect.BoolKind:
+		return Type{IsNamed: true, Name: "boolean"}
+	case
+		protoreflect.Int32Kind,
+		protoreflect.Int64Kind,
+		protoreflect.Uint32Kind,
+		protoreflect.Uint64Kind,
+		protoreflect.DoubleKind,
+		protoreflect.Fixed32Kind,
+		protoreflect.Fixed64Kind,
+		protoreflect.Sfixed32Kind,
+		protoreflect.Sfixed64Kind,
+		protoreflect.FloatKind:
+		return Type{IsNamed: true, Name: "number"}
+	default:
+		return Type{IsNamed: true, Name: "unknown"}
+	}
+}
