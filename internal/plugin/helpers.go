beac9f2d71ceb85e1104ddbc0a8eab0af40a3743
ericwenn <ericwenn@users.noreply.github.com>
2021-03-06 23:04:46 +0100

    refactor: clean up helpers

--- internal/plugin/helpers.go
+++ internal/plugin/helpers.go
@@ -1,7 +1,6 @@
 package plugin
 
 import (
-	"encoding/binary"
 	"strings"
 
 	"google.golang.org/protobuf/reflect/protoreflect"
@@ -46,80 +45,4 @@ func rangeEnumValues(enum protoreflect.EnumDescriptor, f func(value protoreflect
 
 func t(n int) string {
 	return strings.Repeat("\t", n)
-}
-
-type fileSourcePath = int32
-
-const (
-	fileSourcePathMessage fileSourcePath = 4
-	fileSourcePathEnum                   = 5
-)
-
-type messageSourcePath = int32
-
-const (
-	messageSourcePathField   messageSourcePath = 2
-	messageSourcePathMessage                   = 3
-	messageSourcePathEnum                      = 4
-)
-
-type enumSourcePath = int32
-
-const (
-	enumSourcePathValue enumSourcePath = 2
-)
-
-func descriptorSourcePath(desc protoreflect.Descriptor) protoreflect.SourcePath {
-	if _, ok := desc.(protoreflect.FileDescriptor); ok {
-		return nil
-	}
-	var path protoreflect.SourcePath
-	switch desc.Parent().(type) {
-	case protoreflect.FileDescriptor:
-		switch v := desc.(type) {
-		case protoreflect.MessageDescriptor:
-			path = protoreflect.SourcePath{fileSourcePathMessage, int32(v.Index())}
-		case protoreflect.EnumDescriptor:
-			path = protoreflect.SourcePath{fileSourcePathEnum, int32(v.Index())}
-		}
-	case protoreflect.MessageDescriptor:
-		switch v := desc.(type) {
-		case protoreflect.FieldDescriptor:
-			path = protoreflect.SourcePath{messageSourcePathField, int32(v.Index())}
-		case protoreflect.MessageDescriptor:
-			path = protoreflect.SourcePath{messageSourcePathMessage, int32(v.Index())}
-		case protoreflect.EnumDescriptor:
-			path = protoreflect.SourcePath{messageSourcePathEnum, int32(v.Index())}
-		}
-	case protoreflect.EnumDescriptor:
-		switch v := desc.(type) {
-		case protoreflect.EnumValueDescriptor:
-			path = protoreflect.SourcePath{enumSourcePathValue, int32(v.Index())}
-		}
-	}
-	return append(descriptorSourcePath(desc.Parent()), path...)
-}
-
-func descriptorSourceLocation(desc protoreflect.Descriptor, path protoreflect.SourcePath) (protoreflect.SourceLocation, bool) {
-	locs := desc.ParentFile().SourceLocations()
-	key := newPathKey(path)
-	for i := 0; i < locs.Len(); i++ {
-		loc := locs.Get(i)
-		if newPathKey(loc.Path) == key {
-			return loc, true
-		}
-	}
-	return protoreflect.SourceLocation{}, false
-}
-
-// A pathKey is a representation of a location path suitable for use as a map key.
-type pathKey string
-
-// newPathKey converts a location path to a pathKey.
-func newPathKey(path protoreflect.SourcePath) pathKey {
-	buf := make([]byte, 4*len(path))
-	for i, x := range path {
-		binary.LittleEndian.PutUint32(buf[i*4:], uint32(x))
-	}
-	return pathKey(buf)
 }
