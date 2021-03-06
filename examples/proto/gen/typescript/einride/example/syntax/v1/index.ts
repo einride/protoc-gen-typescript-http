da4da7c8010eb7ded0597afa703a2749b6b1362e
ericwenn <ericwenn@users.noreply.github.com>
2021-03-06 13:34:22 +0100

    fix: trim leading whitespace in comments

--- examples/proto/gen/typescript/einride/example/syntax/v1/index.ts
+++ examples/proto/gen/typescript/einride/example/syntax/v1/index.ts
@@ -1,98 +1,98 @@
-//  Message
+// Message
 export type Message = {
-	//  double
+	// double
 	double?: number;
-	//  float
+	// float
 	float?: number;
-	//  int32
+	// int32
 	int32?: number;
-	//  int64
+	// int64
 	int64?: number;
-	//  uint32
+	// uint32
 	uint32?: number;
-	//  uint64
+	// uint64
 	uint64?: number;
-	//  sint32
+	// sint32
 	sint32?: unknown;
-	//  sint64
+	// sint64
 	sint64?: unknown;
-	//  fixed32
+	// fixed32
 	fixed32?: number;
-	//  fixed64
+	// fixed64
 	fixed64?: number;
-	//  sfixed32
+	// sfixed32
 	sfixed32?: number;
-	//  sfixed64
+	// sfixed64
 	sfixed64?: number;
-	//  bool
+	// bool
 	bool?: boolean;
-	//  string
+	// string
 	string?: string;
-	//  bytes
+	// bytes
 	bytes?: string;
-	//  enum
+	// enum
 	enum?: unknown;
-	//  message
+	// message
 	message?: unknown;
-	//  repeated_double
+	// repeated_double
 	repeatedDouble?: number[];
-	//  repeated_float
+	// repeated_float
 	repeatedFloat?: number[];
-	//  repeated_int32
+	// repeated_int32
 	repeatedInt32?: number[];
-	//  repeated_int64
+	// repeated_int64
 	repeatedInt64?: number[];
-	//  repeated_uint32
+	// repeated_uint32
 	repeatedUint32?: number[];
-	//  repeated_uint64
+	// repeated_uint64
 	repeatedUint64?: number[];
-	//  repeated_sint32
+	// repeated_sint32
 	repeatedSint32?: unknown[];
-	//  repeated_sint64
+	// repeated_sint64
 	repeatedSint64?: unknown[];
-	//  repeated_fixed32
+	// repeated_fixed32
 	repeatedFixed32?: number[];
-	//  repeated_fixed64
+	// repeated_fixed64
 	repeatedFixed64?: number[];
-	//  repeated_sfixed32
+	// repeated_sfixed32
 	repeatedSfixed32?: number[];
-	//  repeated_sfixed64
+	// repeated_sfixed64
 	repeatedSfixed64?: number[];
-	//  repeated_bool
+	// repeated_bool
 	repeatedBool?: boolean[];
-	//  repeated_string
+	// repeated_string
 	repeatedString?: string[];
-	//  repeated_bytes
+	// repeated_bytes
 	repeatedBytes?: string[];
-	//  repeated_enum
+	// repeated_enum
 	repeatedEnum?: unknown[];
-	//  repeated_message
+	// repeated_message
 	repeatedMessage?: unknown[];
-	//  map_string_string
+	// map_string_string
 	mapStringString?: unknown[];
-	//  map_string_message
+	// map_string_message
 	mapStringMessage?: unknown[];
-	//  oneof_string
+	// oneof_string
 	oneofString?: string;
-	//  oneof_enum
+	// oneof_enum
 	oneofEnum?: unknown;
-	//  oneof_message1
+	// oneof_message1
 	oneofMessage1?: unknown;
-	//  oneof_message2
+	// oneof_message2
 	oneofMessage2?: unknown;
 };
 
-//  NestedMessage
+// NestedMessage
 export type Message_NestedMessage = {
-	//  nested_message.string
+	// nested_message.string
 	string?: string;
 };
 
-//  Enum
+// Enum
 export type Enum = 
-	//  ENUM_UNSPECIFIED
+	// ENUM_UNSPECIFIED
 	| "ENUM_UNSPECIFIED"
-	//  ENUM_ONE
+	// ENUM_ONE
 	| "ENUM_ONE"
-	//  ENUM_TWO
+	// ENUM_TWO
 	| "ENUM_TWO"
