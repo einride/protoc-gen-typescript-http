4b5f81fbcf4e3b682da76fee0f9c41e261ce4f72
ericwenn <ericwenn@users.noreply.github.com>
2021-07-07 20:21:56 +0200

    feat: generate behavior comments for fields
    `google.api.field_behavior` is used for describing field
    behaviors for fields programmatically, instead of in comments. Because
    they are not in comments, they wont be reflected in the generated
    Typescript code. This change includes an appended behavior comment for
    all fields that are annotated with field_behavior.
    
    Proto3 `optional` do not have this comment appended, because the
    generated types already sufficiently signals this.

--- examples/proto/gen/typescript/einride/example/freight/v1/index.ts
+++ examples/proto/gen/typescript/einride/example/freight/v1/index.ts
@@ -8,25 +8,43 @@ export type Shipment = {
   // The resource name of the shipment.
   name: string | undefined;
   // The creation timestamp of the shipment.
+  //
+  // Behaviors: OUTPUT_ONLY
   createTime: wellKnownTimestamp | undefined;
   // The last update timestamp of the shipment.
   // Updated when create/update/delete operation is shipment.
+  //
+  // Behaviors: OUTPUT_ONLY
   updateTime: wellKnownTimestamp | undefined;
   // The deletion timestamp of the shipment.
+  //
+  // Behaviors: OUTPUT_ONLY
   deleteTime: wellKnownTimestamp | undefined;
   // The resource name of the origin site of the shipment.
   // Format: shippers/{shipper}/sites/{site}
+  //
+  // Behaviors: REQUIRED
   originSite: string | undefined;
   // The resource name of the destination site of the shipment.
   // Format: shippers/{shipper}/sites/{site}
+  //
+  // Behaviors: REQUIRED
   destinationSite: string | undefined;
   // The earliest pickup time of the shipment at the origin site.
+  //
+  // Behaviors: REQUIRED
   pickupEarliestTime: wellKnownTimestamp | undefined;
   // The latest pickup time of the shipment at the origin site.
+  //
+  // Behaviors: REQUIRED
   pickupLatestTime: wellKnownTimestamp | undefined;
   // The earliest delivery time of the shipment at the destination site.
+  //
+  // Behaviors: REQUIRED
   deliveryEarliestTime: wellKnownTimestamp | undefined;
   // The latest delivery time of the shipment at the destination site.
+  //
+  // Behaviors: REQUIRED
   deliveryLatestTime: wellKnownTimestamp | undefined;
   // The line items of the shipment.
   lineItems: LineItem[] | undefined;
@@ -56,13 +74,21 @@ export type Shipper = {
   // The resource name of the shipper.
   name: string | undefined;
   // The creation timestamp of the shipper.
+  //
+  // Behaviors: OUTPUT_ONLY
   createTime: wellKnownTimestamp | undefined;
   // The last update timestamp of the shipper.
   // Updated when create/update/delete operation is performed.
+  //
+  // Behaviors: OUTPUT_ONLY
   updateTime: wellKnownTimestamp | undefined;
   // The deletion timestamp of the shipper.
+  //
+  // Behaviors: OUTPUT_ONLY
   deleteTime: wellKnownTimestamp | undefined;
   // The display name of the shipper.
+  //
+  // Behaviors: REQUIRED
   displayName: string | undefined;
 };
 
@@ -72,13 +98,21 @@ export type Site = {
   // The resource name of the site.
   name: string | undefined;
   // The creation timestamp of the site.
+  //
+  // Behaviors: OUTPUT_ONLY
   createTime: wellKnownTimestamp | undefined;
   // The last update timestamp of the site.
   // Updated when create/update/delete operation is performed.
+  //
+  // Behaviors: OUTPUT_ONLY
   updateTime: wellKnownTimestamp | undefined;
   // The deletion timestamp of the site.
+  //
+  // Behaviors: OUTPUT_ONLY
   deleteTime: wellKnownTimestamp | undefined;
   // The display name of the site.
+  //
+  // Behaviors: REQUIRED
   displayName: string | undefined;
   // The geographic location of the site.
   latLng: googletype_LatLng | undefined;
@@ -100,6 +134,8 @@ export type googletype_LatLng = {
 export type GetShipperRequest = {
   // The resource name of the shipper to retrieve.
   // Format: shippers/{shipper}
+  //
+  // Behaviors: REQUIRED
   name: string | undefined;
 };
 
@@ -129,6 +165,8 @@ export type ListShippersResponse = {
 // Request message for FreightService.CreateShipper.
 export type CreateShipperRequest = {
   // The shipper to create.
+  //
+  // Behaviors: REQUIRED
   shipper: Shipper | undefined;
 };
 
@@ -137,6 +175,8 @@ export type UpdateShipperRequest = {
   // The shipper to update with. The name must match or be empty.
   // The shipper's `name` field is used to identify the shipper to be updated.
   // Format: shippers/{shipper}
+  //
+  // Behaviors: REQUIRED
   shipper: Shipper | undefined;
   // The list of fields to be updated.
   updateMask: wellKnownFieldMask | undefined;
@@ -174,6 +214,8 @@ type wellKnownFieldMask = string;
 export type DeleteShipperRequest = {
   // The resource name of the shipper to delete.
   // Format: shippers/{shipper}
+  //
+  // Behaviors: REQUIRED
   name: string | undefined;
 };
 
@@ -181,6 +223,8 @@ export type DeleteShipperRequest = {
 export type GetSiteRequest = {
   // The resource name of the site to retrieve.
   // Format: shippers/{shipper}/sites/{site}
+  //
+  // Behaviors: REQUIRED
   name: string | undefined;
 };
 
@@ -188,6 +232,8 @@ export type GetSiteRequest = {
 export type ListSitesRequest = {
   // The resource name of the parent, which owns this collection of sites.
   // Format: shippers/{shipper}
+  //
+  // Behaviors: REQUIRED
   parent: string | undefined;
   // Requested page size. Server may return fewer sites than requested.
   // If unspecified, server will pick an appropriate default.
@@ -214,8 +260,12 @@ export type ListSitesResponse = {
 export type CreateSiteRequest = {
   // The resource name of the parent shipper for which this site will be created.
   // Format: shippers/{shipper}
+  //
+  // Behaviors: REQUIRED
   parent: string | undefined;
   // The site to create.
+  //
+  // Behaviors: REQUIRED
   site: Site | undefined;
 };
 
@@ -224,6 +274,8 @@ export type UpdateSiteRequest = {
   // The site to update with. The name must match or be empty.
   // The site's `name` field is used to identify the site to be updated.
   // Format: shippers/{shipper}/sites/{site}
+  //
+  // Behaviors: REQUIRED
   site: Site | undefined;
   // The list of fields to be updated.
   updateMask: wellKnownFieldMask | undefined;
@@ -233,6 +285,8 @@ export type UpdateSiteRequest = {
 export type DeleteSiteRequest = {
   // The resource name of the site to delete.
   // Format: shippers/{shipper}/sites/{site}
+  //
+  // Behaviors: REQUIRED
   name: string | undefined;
 };
 
@@ -240,6 +294,8 @@ export type DeleteSiteRequest = {
 export type GetShipmentRequest = {
   // The resource name of the shipment to retrieve.
   // Format: shippers/{shipper}/shipments/{shipment}
+  //
+  // Behaviors: REQUIRED
   name: string | undefined;
 };
 
@@ -247,6 +303,8 @@ export type GetShipmentRequest = {
 export type ListShipmentsRequest = {
   // The resource name of the parent, which owns this collection of shipments.
   // Format: shippers/{shipper}
+  //
+  // Behaviors: REQUIRED
   parent: string | undefined;
   // Requested page size. Server may return fewer shipments than requested.
   // If unspecified, server will pick an appropriate default.
@@ -273,8 +331,12 @@ export type ListShipmentsResponse = {
 export type CreateShipmentRequest = {
   // The resource name of the parent shipper for which this shipment will be created.
   // Format: shippers/{shipper}
+  //
+  // Behaviors: REQUIRED
   parent: string | undefined;
   // The shipment to create.
+  //
+  // Behaviors: REQUIRED
   shipment: Shipment | undefined;
 };
 
@@ -283,6 +345,8 @@ export type UpdateShipmentRequest = {
   // The shipment to update with. The name must match or be empty.
   // The shipment's `name` field is used to identify the shipment to be updated.
   // Format: shippers/{shipper}/shipments/{shipment}
+  //
+  // Behaviors: REQUIRED
   shipment: Shipment | undefined;
   // The list of fields to be updated.
   updateMask: wellKnownFieldMask | undefined;
@@ -292,6 +356,8 @@ export type UpdateShipmentRequest = {
 export type DeleteShipmentRequest = {
   // The resource name of the shipment to delete.
   // Format: shippers/{shipper}/shipments/{shipment}
+  //
+  // Behaviors: REQUIRED
   name: string | undefined;
 };
 
