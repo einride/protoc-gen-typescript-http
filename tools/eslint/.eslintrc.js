290a799457f0b3705dee05689cab15d90ca2702b
Filip Tammergård <filip@tammergard.se>
2021-12-16 11:42:07 +0100

    chore(deps): bump eslint, eslint-plugin and node version in CI

--- tools/eslint/.eslintrc.js
+++ tools/eslint/.eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
   extends: ["plugin:@einride/default"],
-    rules: {
+  rules: {
     "jest/no-deprecated-functions": 0,
     "prettier/prettier": 0,
+    "prefer-template": 0,
+    "@typescript-eslint/no-unused-vars": 0,
+    "no-restricted-syntax": 0,
   },
 };
