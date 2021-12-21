860ec09ada86f6d4d33ece724f3e3d5e2418fbfb
Filip Tammergård <filip@tammergard.se>
2021-12-21 11:50:18 +0100

    chore: clarify why certain ESLint rules are globally disabled

--- tools/eslint/.eslintrc.js
+++ tools/eslint/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ["plugin:@einride/default"],
   rules: {
-    "jest/no-deprecated-functions": 0,
-    "prettier/prettier": 0,
+    "jest/no-deprecated-functions": "off", // we're not using Jest
+    "prettier/prettier": "off", // we're not concerned with code style
   },
 };
