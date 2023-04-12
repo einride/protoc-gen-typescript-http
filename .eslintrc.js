438a4a8f74d823042f954d95c7b6b865d6b4c659
Filip Tammergård <filip@tammergard.se>
2023-04-12 13:42:43 +0200

    fix: disable type checking in generated files

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
   rules: {
     "jest/no-deprecated-functions": "off", // we're not using Jest
     "prettier/prettier": "off", // we're not concerned with code style
+    "@typescript-eslint/ban-ts-comment": "off", // we need ts comment in generated files to disable type checking of files for consumers
   },
 };
